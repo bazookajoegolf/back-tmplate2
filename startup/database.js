cae502a7a2cdf55e55c2708b29bbf427f2ccd5ab
bazookajoegolf <bazookajoegolf@outlook.com>
2024-12-18 15:02:02 -0700

    dec18-2024

--- startup/database.js
+++ startup/database.js
@@ -2,13 +2,15 @@
 const config = require('config');    
 const mongoose = require('mongoose');
 const winston = require('winston');
+const db = process.env.DBSTRING;
 
 module.exports = function () {
 
 // use config to define db connection string  winston.info('Connected to Mongodb')
 //mongoose.connect(config.db, {useNewUrlParser : true, useCreateIndexes : true })
 mongoose.set('strictQuery',false);
-mongoose.connect('mongodb+srv://mongoservice:MoBinf0rd1@cluster0.dxjt8.mongodb.net/golfhandicap?retryWrites=true&w=majority');
+//mongoose.connect('mongodb+srv://mongoservice:MoBinf0rd1@cluster0.dxjt8.mongodb.net/golfhandicap?retryWrites=true&w=majority');
+mongoose.connect(db);
 mongoose.connection.on('connected', () => console.log('Connected'));
 mongoose.connection.on('error', () => console.log('Connection failed with - ',err))
 
